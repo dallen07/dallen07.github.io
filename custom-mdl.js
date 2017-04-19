df6c9790d46f88f9402992ec53a063f776ddfa54
Drew Allen <drew.allen.3b.uhsa@gmail.com>
2017-04-19 15:42:49 -0400

    Update custom-mdl.js

--- custom-mdl.js
+++ custom-mdl.js
@@ -1,19 +1,15 @@
-(function() 
- {
-  'use strict';
-  var dialog = document.querySelector('#curr_projs-dialog');
-  var dialogButton = document.querySelector('.curr_projs_button');
-  
-  
-   dialogButton.registerPolyfill(dialog);
-  
-  
-  dialogButton.addEventListener('click', function() {
-   dialog.showModal();
-  });
-  
-  dialogButton.querySelector('.close_button')
-  .addEventListener('click', function() {
-   dialog.close();
-  });
- }());
+(function() {
+    'use strict';
+    var dialogButton = document.querySelector('.curr_projs_button');
+    var dialog = document.querySelector('#curr_projs_dialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialogButton.addEventListener('click', function() {
+       dialog.showModal();
+    });
+    dialog.querySelector('button:not([disabled])')
+    .addEventListener('click', function() {
+      dialog.close();
+    });
+  }());
