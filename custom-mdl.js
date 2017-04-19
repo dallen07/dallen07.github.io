fe385e945b05bacede3ec12c7c89c10ab62ec8bb
Drew Allen <drew.allen.3b.uhsa@gmail.com>
2017-04-19 15:37:31 -0400

    Update custom-mdl.js

--- custom-mdl.js
+++ custom-mdl.js
@@ -4,10 +4,9 @@
   var dialog = document.querySelector('#curr_projs-dialog');
   var dialogButton = document.querySelector('.curr_projs_button');
   
-  if(!dialog.showModal)
-  {
+  
    dialogButton.registerPolyfill(dialog);
-  }
+  
   
   dialogButton.addEventListener('click', function() {
    dialog.showModal();
