81e57cce15e2b6d94c4ca6acdddbf692a01592d4
Drew Allen <drew.allen.3b.uhsa@gmail.com>
2017-04-24 12:21:17 -0400

    Updated to a for loop

--- custom-mdl.js
+++ custom-mdl.js
@@ -1,4 +1,4 @@
-(function ()
+/*(function ()
  {
  var dialogButton = document.querySelectorAll('.expand_button');
  var dialog = document.querySelectorAll('.mdl-dialog');
@@ -14,4 +14,25 @@
     .addEventListener('click', function() {
       dialog.close();
     });
-}());
+}());*/
+
+(function ()
+{
+ var dialogs = document.querySelectorAll('.dialog-container'), i;
+ for (i = 0; i < dialogs.length; i++)
+ {
+  var dialog = dialogs[i].querySelector('.mdl-dialog'),
+      expandButton = dialogs[i].querySelector('.expand_button'),
+      closeButton = dialogs[i].querySelector('.close_button');
+  
+  dialogPolyfill.registerDialog(dialogs[i]));
+  
+  expandButton.addEventListener('click', function () {
+    this.showModal();
+  }.bind(dialog));
+  
+  closeButton.addEventListener('click', function () {
+    this.showModal();
+  }.bind(dialog));
+ }
+}
